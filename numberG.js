ac04af13fe2f7f566bbf95b04d827270c3bf63a8
0lgaP <perronolga@gmail.com>
2021-12-10 22:28:04 +0000

    first working node game

--- numberG.js
+++ numberG.js
@@ -1,35 +1,37 @@
 let prompt = require("prompt-sync")();
 
-// code below (replace this example)
-let number = Math.floor(Math.random() * 10);
-let answer = prompt("Guess a number: ");
-let chances = 10;
+let secretNumber = Math.floor(Math.random() * 100);
 
-
-//is number correct loop
 function checkNumber(num) {
-  
-  for (let i = 0; i < chances; i++) {
-    if (num < number) {
-      chances -= 1
+  let answer = prompt("Guess a number: ");
+  let numbersTried = [];
+  let chances = 10;
+  for (let i = 0; i < 9; i++) {
+    if (num == answer) {
+      return ("Nailed it, the number I wanted is " + answer + " it only took " + chances + " attempts")
+    }
+    else if (isNaN(answer)) {
+      console.log("I'm not sure this is a number, please enter a number form 1 to 100")
+      console.log("you have " + chances + " chances remaining")
+      answer = prompt("Guess a number: ")
+    } else if (numbersTried.includes(answer)) {
+      console.log("Ya'll tried that number already")
+      answer = prompt("Guess a number: ");
+    } else if (num > answer) {
+      chances = chances - 1
+      numbersTried.push(answer)
       console.log("The number I am thinking of is higher than " + answer)
       console.log("you have " + chances + " chances remaining")
       answer = prompt("Guess a number: ");
-    } else if (num > number) {
-      chances -= 1
+    } else if (num < answer) {
+      chances = chances - 1
+      numbersTried.push(answer)
       console.log("The number I am thinking of is lower than " + answer)
       console.log("you have " + chances + " chances remaining")
       answer = prompt("Guess a number: ");
-    } else if (typeof num === NaN) {
-      console.log("I'm not sure this is a number, please enter a number form 1 to 100")
-      console.log("you have " + chances + " chances remaining")
-      answer = prompt("Guess a number: ");
-    } else if(num === answer) {
-      console.log("Nailed it, the number I wanted is " + answer)
-      break;
+    } else if (num === answer) {
+      return ("Nailed it, the number I wanted is " + answer + "It only took " + chances + " attempts")
     }
-  }
+  } return ("Thanks for playing!")
 }
-
-
-console.log(checkNumber(answer));
+console.log(checkNumber(secretNumber));
