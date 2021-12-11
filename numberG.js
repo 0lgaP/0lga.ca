4b5feddf6a5dcc417cdbc9cdec28d31189b10f4b
0lgaP <perronolga@gmail.com>
2021-12-11 20:56:52 +0000

    number game improvements & some strange fork stuff

--- numberG.js
+++ numberG.js
@@ -3,25 +3,38 @@ let prompt = require("prompt-sync")();
 let secretNumber = Math.floor(Math.random() * 100);
 
 function checkNumber(num) {
+
   let answer = prompt("Guess a number: ");
   let numbersTried = [];
   let chances = 10;
   for (let i = 0; i < 9; i++) {
+
     if (num == answer) {
-      return ("Nailed it, the number I wanted is " + answer + " it only took " + chances + " attempts")
-    }
-    else if (isNaN(answer)) {
-      console.log("I'm not sure this is a number, please enter a number form 1 to 100")
-      console.log("you have " + chances + " chances remaining")
+      if (i === 0) {
+        return ("BOOM FIRST TRY BABY!!!!! I wanted " + answer + " and you got it on your first try!" + "\n" + "Thanks for playing :)")
+      } else if (i === 1) {
+        return ("WOW GOT IT ON YOUR 2ND TRY! I wanted " + answer + " and you got it!" + "\n" + "Thanks for playing :)")
+      } else if (i === 2) {
+        return ("Third time's the charm!" + "I wanted " + answer + "!" + "\n" + "Thanks for playing :)")
+      } else if (i === 8) {
+        return ("Close one! You guessed correctly on your last chance!" + answer + " is the number I wanted" + "\n" + "Thanks for playing :)")
+      } else {
+        return ("Nailed it! The number I wanted is " + answer + " it only took you " + (i + 1) + " attempts to guess correctly!" + "\n" + "Thanks for playing :)")
+      }
+    } else if (isNaN(answer)) {
+      console.log("I'm not convinced " + answer + " is a number, please enter a positional notation digit from 1 to 100")
+      console.log("You have " + chances + " chances remaining")
       answer = prompt("Guess a number: ")
     } else if (numbersTried.includes(answer)) {
       console.log("Ya'll tried that number already")
       answer = prompt("Guess a number: ");
+    } else if (i === 8 && num != answer) {
+      console.log("You loose ... looser" + "\n" + "JKJK!" + "\n" + "Thanks for playing :)")
     } else if (num > answer) {
       chances = chances - 1
       numbersTried.push(answer)
       console.log("The number I am thinking of is higher than " + answer)
-      console.log("you have " + chances + " chances remaining")
+      console.log("You have " + chances + " chances remaining")
       answer = prompt("Guess a number: ");
     } else if (num < answer) {
       chances = chances - 1
@@ -29,9 +42,11 @@ function checkNumber(num) {
       console.log("The number I am thinking of is lower than " + answer)
       console.log("you have " + chances + " chances remaining")
       answer = prompt("Guess a number: ");
-    } else if (num === answer) {
-      return ("Nailed it, the number I wanted is " + answer + "It only took " + chances + " attempts")
     }
-  } return ("Thanks for playing!")
+  }
+  return "This game was made by 0lgatron"
 }
+
+
+
 console.log(checkNumber(secretNumber));
